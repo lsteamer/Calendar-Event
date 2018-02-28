ffbddfefe551d74788a6411093806f077826fa03
lsteamer <lsteamer@gmail.com>
2018-02-28 19:36:42 +0100

    07 - Cleaned new Activity layout (EventDetailActivity)

--- app/src/main/java/lsteamer/elmexicano/com/calendarmarker/EventDetailActivity.java
+++ app/src/main/java/lsteamer/elmexicano/com/calendarmarker/EventDetailActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import lsteamer.elmexicano.com.calendarmarker.utils.ColorUtil;
 
 /**
  * Created by lsteamer on 28/02/2018.
@@ -15,6 +18,7 @@ public class EventDetailActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_detail);
+
 
     }
 }
